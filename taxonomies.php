db45c8256b96ccdc6d3c225d4acebe1e1a4c7dae
Oleg Polyakov <olegpolyakov@outlook.com>
2024-02-06 20:27:42 -0300

    Add teacher_type taxonomy

--- taxonomies.php
+++ taxonomies.php
@@ -38,6 +38,34 @@ $taxonomies = [
         ]
     ],
 
+    'teacher_type' => [
+        ['teacher'],
+        [
+            'label' => esc_html__( 'Тип преподавателя', 'custom-post-type-ui' ),
+            'labels' => [
+                'name' => esc_html__( 'Тип преподавателя', 'custom-post-type-ui' ),
+                'singular_name' => esc_html__( 'Тип преподавателя', 'custom-post-type-ui' ),
+            ],
+            'public' => true,
+            'publicly_queryable' => true,
+            'hierarchical' => true,
+            'show_ui' => true,
+            'show_in_menu' => true,
+            'show_in_nav_menus' => true,
+            'query_var' => true,
+            'rewrite' => [ 'slug' => 'teacher_type', 'with_front' => true, ],
+            'show_admin_column' => true,
+            'show_in_rest' => false,
+            'show_tagcloud' => true,
+            'rest_base' => 'teacher_type',
+            'rest_controller_class' => 'WP_REST_Terms_Controller',
+            'rest_namespace' => 'wp/v2',
+            'show_in_quick_edit' => true,
+            'sort' => false,
+            'show_in_graphql' => false,
+        ]
+    ],
+
     'testimonial_type' => [
         ['testimonial'],
         [
