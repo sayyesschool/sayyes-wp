ecbb566e681f72142689f6d774bea246d011faf6
Oleg Polyakov <olegpolyakov@outlook.com>
2024-02-06 20:27:50 -0300

    Update teachers page

--- page-teachers.php
+++ page-teachers.php
@@ -15,10 +15,10 @@ $teachers = Timber::get_posts([
 ])->to_array();
 
 $context['offline_teachers'] = array_filter($teachers, function($teacher) {
-    return empty($teacher->skype);
+    return !$teacher->has_term('online');
 });
 $context['online_teachers'] = array_filter($teachers, function($teacher) {
-    return !empty($teacher->skype);
+    return $teacher->has_term('online');
 });
 
 Timber::render('pages/teachers.twig', $context);
