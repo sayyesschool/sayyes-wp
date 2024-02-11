173ed6bdcf731e720f482692d94f4c156659d12a
Oleg Polyakov <olegpolyakov@outlook.com>
2024-02-11 15:00:17 -0300

    Update contact page

--- page-contact.php
+++ page-contact.php
@@ -8,6 +8,11 @@ $context['page'] = Timber::get_post();
 $context['managers'] = Timber::get_posts([
     'post_type' => 'manager',
     'posts_per_page' => -1,
+    'post_name__in' => [
+        'роман-сорочук',
+        'анастасия-жителева',
+        'татьяна-андреева'
+    ],
     'orderby' => 'menu_order',
     'order' => 'ASC'
 ]);
