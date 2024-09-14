f196322e4bdf77d4e048d9e4dba253e00a06ea2e
Oleg Polyakov <olegpolyakov@outlook.com>
2024-09-14 16:04:30 +0800

    Update version

--- site.php
+++ site.php
@@ -11,7 +11,7 @@ require 'constants.php';
 require 'utils.php';
 
 class SayYesSite extends Site {
-    public $version = '2.3.0';
+    public $version = '3.0.0-rc.0';
 
 	function __construct() {
         // add_theme_support('html5');
