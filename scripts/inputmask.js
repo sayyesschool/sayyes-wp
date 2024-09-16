097ba30fae9e0949326e525cefc51d2a68de94e9
Oleg Polyakov <olegpolyakov@outlook.com>
2024-09-16 09:37:26 +0800

    Update

--- scripts/inputmask.js
+++ scripts/inputmask.js
@@ -1,10 +1,18 @@
-// import Inputmask from 'inputmask';
 import intlTelInput from 'intl-tel-input/intlTelInputWithUtils';
+import ru from 'intl-tel-input/i18n/ru';
 
-// document.querySelectorAll('input[type=tel]')
-//     .forEach(element => Inputmask('+7 (999) 999-9999').mask(element));
 
 document.querySelectorAll('input[type=tel]')
     .forEach(input => intlTelInput(input, {
-        initialCountry: 'ru'
+        autoPlaceholder: 'aggressive',
+        initialCountry: 'ru',
+        i18n: ru,
+        formatAsYouType: true,
+        strictMode: true,
+        hiddenInput: telInputName => ({
+            phone: 'phone',
+            country: 'country'
+        }),
+        customPlaceholder: (selectedCountryPlaceholder, selectedCountryData) =>
+            selectedCountryPlaceholder + '*',
     }));
