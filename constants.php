765622b718ff3ebb8db8544ec4edf44ca26c2a4f
Oleg Polyakov <olegpolyakov@outlook.com>
2024-10-23 14:28:31 -0300

    Update

--- constants.php
+++ constants.php
@@ -7,7 +7,7 @@ define('COMPANY_AGE', date('Y') - COMPANY_YEAR);
 define('JIVOSITE_KEY', 'N60okSsqRQ');
 
 define('SITE_DOMAIN', 'sayes.ru');
-define('SITE_EMAIL', 'site@sayyes.school');
+define('SITE_EMAIL', 'site@sayes.ru');
 define('REQUEST_EMAIL', 'request@sayyes.school');
 define('CRM_REQUEST_URL', 'https://sayes.t8s.ru/Api/V2/AddStudyRequest');
 
