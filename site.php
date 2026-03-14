12c08b242720b09f9b4840c280c772d495cff62d
Oleg Polyakov <olegpolyakov@outlook.com>
2026-03-14 16:09:13 +0800

    fix: update version number to 3.1.0

--- site.php
+++ site.php
@@ -11,7 +11,7 @@ require_once 'constants.php';
 require 'utils.php';
 
 class SayYesSite extends Site {
-    public $version = '3.0.2';
+    public $version = '3.1.0';
 
 	function __construct() {
         // add_theme_support('html5');
